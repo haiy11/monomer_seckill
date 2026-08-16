df18391beb02540e45e8ac7b0b65d5537505bfcd
刘海阳 <244861625@qq.com>
2026-08-16 18:15:43 +0800

    后端初始化

---
+++ monomer_seckill_backend/monomer_seckill_backend/src/main/java/org/example/monomer_seckill_backend/MonomerSeckillBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.monomer_seckill_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonomerSeckillBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonomerSeckillBackendApplication.class, args);
+	}
+
+}
---
+++ monomer_seckill_backend/monomer_seckill_backend/src/test/java/org/example/monomer_seckill_backend/MonomerSeckillBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.monomer_seckill_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonomerSeckillBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
